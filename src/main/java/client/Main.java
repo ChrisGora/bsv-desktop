fca540e62ad7f6fae6deda3cbb7a180a14d025f7
Chris Gora <kg17815@my.bristol.ac.uk>
2018-07-13 12:59:39 +0100

    Simple button that calls the test method

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,9 +1,31 @@
 package client;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Uploader uploader;
 
     public static void main(String[] args) {
-        Uploader uploader = new Uploader();
-        uploader.test();
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        this.uploader = new Uploader();
+        primaryStage.setTitle("Database client");
+
+        Button button = new Button();
+        button.setText("Upload file");
+        button.setOnAction((event) -> uploader.test());
+
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+
+        primaryStage.setScene(new Scene(root, 300, 300));
+        primaryStage.show();
     }
 }
--- src/main/java/client/S3Connector.java
+++ src/main/java/client/S3Connector.java
@@ -8,6 +8,10 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+
+import java.io.File;
 
 class S3Connector {
 
@@ -16,8 +20,8 @@ class S3Connector {
     private String key;
 
     S3Connector(Regions region, String bucketName) {
-        AWSCredentialsProvider awsCredentialsProvider = new AWSCredentialsProviderChain();
-        awsCredentialsProvider.getCredentials();
+//        AWSCredentialsProvider awsCredentialsProvider = new AWSCredentialsProviderChain();
+//        awsCredentialsProvider.getCredentials();
         this.s3 = AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .build();
@@ -28,7 +32,16 @@ class S3Connector {
     public void listBuckets() {
         for (Bucket bucket : s3.listBuckets()) {
             System.out.println("Found bucket: " + bucket.getName());
+
         }
+    }
+
+    // Needs to upload a photo and return some sort of reference (a url?)
+    public void uploadFile(String key, File file) {
+        PutObjectRequest request = new PutObjectRequest(bucketName, key, file);
+        PutObjectResult result = s3.putObject(request);
+        System.out.println(result);
+//        return "";
     }
 
 }
