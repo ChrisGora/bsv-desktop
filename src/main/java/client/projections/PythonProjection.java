7003124e5d7991a644789822b55424137cf22009
Chris Gora <kg17815@my.bristol.ac.uk>
2018-08-27 22:00:23 +0100

    python projection tool improvements

---
+++ src/main/java/client/projections/PythonProjection.java
@@ -0,0 +1,34 @@
+package client.projections;
+
+import org.python.core.PyObject;
+import org.python.util.PythonInterpreter;
+
+import java.util.StringTokenizer;
+
+public class PythonProjection {
+
+    public void run() {
+
+        String property = System.getProperty("java.library.path");
+        StringTokenizer parser = new StringTokenizer(property, ":");
+
+        while (parser.hasMoreTokens()) {
+            System.out.println(parser.nextToken());
+        }
+
+//        System.loadLibrary("JyNI");
+//        System.loadLibrary("JyNI-Loader");
+
+        try (PythonInterpreter python = new PythonInterpreter()) {
+
+            python.exec("import sys");
+            python.exec("print sys");
+
+//            python.exec("pip");
+
+            python.exec("import numpy as py");
+        }
+//        python.exec("execfile('src/main/python/equirectangular-toolbox/nfov.py')");
+    }
+
+}
---
+++ src/test/java/client/projections/PythonProjectionTest.java
@@ -0,0 +1,13 @@
+package client.projections;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PythonProjectionTest {
+
+    @Test
+    public void run() {
+        new PythonProjection().run();
+    }
+}
