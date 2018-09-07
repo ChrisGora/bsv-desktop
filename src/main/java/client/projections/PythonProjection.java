f05b4a1a4e85eaaa6ff453378aa3029558cd637c
Chris Gora <kg17815@my.bristol.ac.uk>
2018-09-07 18:38:13 +0100

    test

--- src/main/java/client/projections/PythonProjection.java
+++ src/main/java/client/projections/PythonProjection.java
@@ -17,7 +17,7 @@ public class PythonProjection {
             System.out.println(parser.nextToken());
         }
 
-tes
+te
 
 //        System.loadLibrary("JyNI");
 //        System.loadLibrary("JyNI-Loader");
