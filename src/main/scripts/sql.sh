44c344b42b4725bdd7d7d2ff6589649ab9fb2a46
Chris Gora <kg17815@my.bristol.ac.uk>
2018-09-21 12:00:00 +0100

    Very fina improvements

--- src/main/scripts/sql.sh
+++ src/main/scripts/sql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -N -u java-db-client -p  < script.sql > out.txt
+mysql -N -u java-db-client -pRe278nErRowD  < script.sql > out.txt
 java -jar $HOME/client/client.jar -b=$1 -vepf=$HOME/client/log.txt -s @out.txt
--- src/main/scripts/upload.sh
+++ src/main/scripts/upload.sh
@@ -2,15 +2,19 @@
 
 
 ./backup.sh $1
+
+BACK="$(pwd)"
+
 cd /run/user/$UID/gvfs/mtp*/
 cd Internal\ storage/Ricoh/
 STR="$(pwd)"
 STR="$(echo -e "\x22$STR\x22")"
-echo $STR
+echo ${STR}
 
 if [ -a log.txt ]
 then
     rm log.txt
 fi
 
+cd ${BACK}
 java -jar client.jar -b=$1 -vepf=log.txt -r="$2" -gu="$STR"
